fce68002efce9b5fdfb4f93c46ab72052a461dbd
Lư Nguyễn Hoài Chung <xboxonevn@gmail.com>
2026-01-19 01:33:57 +0700

    Remove unused imports and asset directories
    Deleted an unused import from content_database.dart and removed the assets/db and assets/paths directories from pubspec.yaml assets. This cleans up unused resources and streamlines the project configuration.

--- lib/data/db/content_database.dart
+++ lib/data/db/content_database.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
-import 'package:flutter/services.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
